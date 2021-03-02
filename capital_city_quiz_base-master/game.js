8b6da6cac7d54c80cd807502290c4d35b8bef828
clara <10088152+claraj@users.noreply.github.com>
2021-03-02 12:24:58 -0600

    add event handler outside game function

--- capital_city_quiz_base-master/game.js
+++ capital_city_quiz_base-master/game.js
@@ -13,24 +13,27 @@ let startAgain = document.querySelector('#start-again')
 //console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
 
 
+let answer
+let city
+let countryName
+
 // TODO when the page loads, select an element at random from the countriesAndCodes array
 
 // putting almost everything in a function to call again
-function checkAnswer(){
-// from https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
- let random = Math.floor(Math.random() * countriesAndCodes.length)
-// TODO display the country's name in the randomCountryElement 
+// renamed this function because it sets up one round of the game 
+function startGame(){
+  // from https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+  let random = Math.floor(Math.random() * countriesAndCodes.length)
+  // TODO display the country's name in the randomCountryElement 
 
-let country = countriesAndCodes[random].name
-// displaying the country to guess on the page
-randomCountryElement.innerHTML = country
-// getting the country code of the country to guess
-let countryCode = countriesAndCodes[random]["alpha-2"]
+  let country = countriesAndCodes[random].name
+  // displaying the country to guess on the page
+  randomCountryElement.innerHTML = country
+  // getting the country code of the country to guess
+  let countryCode = countriesAndCodes[random]["alpha-2"]
 
-// checking api for the correct info based on country code
-let url = ` https://api.worldbank.org/v2/country/${countryCode}?format=json`
-
-
+  // checking api for the correct info based on country code
+  let url = ` https://api.worldbank.org/v2/country/${countryCode}?format=json`
 
 
 // TODO add a click event handler to the submitButton.  When the user clicks the button,
@@ -46,8 +49,6 @@ let url = ` https://api.worldbank.org/v2/country/${countryCode}?format=json`
 //      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
 
 
-
-  
     // still part of function
     // gets the info from api and puts into JSON then extracts the info needed    
     fetch(url).then((res) =>{
@@ -56,45 +57,44 @@ let url = ` https://api.worldbank.org/v2/country/${countryCode}?format=json`
     }).then((capitol) =>{
         console.log(capitol)
         // I got help from a tutor and this is how he extracted info I got it without using forEach
-        let answer
-        let city
-        let countryName
+
         capitol.forEach(function(){
           // getting the city name
             city = capitol[1][0].capitalCity
-            // putting user input into javascript
-            answer = userAnswerElement.value
+
             // getting country name
              countryName = capitol[1][0].name
             // the tutor helped me with the suggestion of putting the event listener here
-            submitButton.addEventListener('click', function(){
-                // checkAnswer()
-                if(city.toUpperCase() == answer.toUpperCase()){
-                    resultTextElement.innerHTML = `Correct the capital of ${countryName} is ${city}`
-                     }else {
-                    resultTextElement.innerHTML = `Wrong the capital of ${countryName} is ${city}`
-                    }
-                 })
-                })
+            // adding event listener moved outside this function
+        })
        }).catch((err) =>{
         console.log('!Error ', err)
       })
 
 }
 // calling function to begin program
-checkAnswer()
+startGame()
 startAgain.addEventListener('click', function(){
   // clearing fields and calling function again if try again button is pressed
     userAnswerElement.value = ''
     resultTextElement.innerHTML = ''
-    checkAnswer()
+    startGame()
  })
+
+
+ // event listener added one time
+submitButton.addEventListener('click', function(){
+  // putting user input into javascript
+  answer = userAnswerElement.value
+  
+  if(city.toUpperCase() == answer.toUpperCase()){
+      resultTextElement.innerHTML = `Correct the capital of ${countryName} is ${city}`
+  } else {
+      resultTextElement.innerHTML = `Wrong the capital of ${countryName} is ${city}`
+  }
+})
+  
     
-    
-
-
-
-
 
 // TODO finally, connect the play again button. Clear the user's answer, select a new random country, 
 // display the country's name, handle the user's guess. If you didn't use functions in the code you've 
